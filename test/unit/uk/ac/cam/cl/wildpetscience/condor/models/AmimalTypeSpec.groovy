10ec89322df301298c1cefa80af273e973955feb
Nick Rogers <rogersnm@hotmail.co.uk>
2015-02-10 12:07:40 +0000

    Fix typo

--- test/unit/uk/ac/cam/cl/wildpetscience/condor/models/AmimalTypeSpec.groovy
+++ test/unit/uk/ac/cam/cl/wildpetscience/condor/models/AmimalTypeSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(AmimalType)
-class AmimalTypeSpec extends Specification {
+@TestFor(AnimalType)
+class AnimalTypeSpec extends Specification {
 
     def setup() {
     }
