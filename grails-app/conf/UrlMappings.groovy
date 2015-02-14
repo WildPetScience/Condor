10ab50953d06a9d1a1362a2772f5bc2beb2f2d4e
Nick Rogers <rogersnm@hotmail.co.uk>
2015-02-14 10:30:05 +0000

    Configure endpoints

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,20 +6,20 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-		
-		"/clients"(resources:"Client") {
-            "/positions"(controller:"Positions", method:"GET", action:"index")
-            "/positions"(controller:"Positions", method:"POST", action:"save")
-            "/positions"(controller:"Positions", method:"PUT", action:"update")
-            "/positions"(controller:"Positions", method:"DELETE", action:"delete")
-            "/zones"(controller:"Positions", method:"GET", action:"index")
-            "/zones"(controller:"Positions", method:"POST", action:"save")
-            "/zones"(controller:"Positions", method:"PUT", action:"update")
-            "/zones"(controller:"Positions", method:"DELETE", action:"delete")
+
+        "/clients"(resources:"Client", excludes:['create', 'show', 'edit', 'patch']) {
+            "/positions"(controller:"Position", method:"GET", action:"index")
+            "/positions"(controller:"Position", method:"POST", action:"save")
+            "/positions"(controller:"Position", method:"PUT", action:"update")
+            "/positions"(controller:"Position", method:"DELETE", action:"delete")
+            "/zones"(controller:"Zone", method:"GET", action:"index")
+            "/zones"(controller:"Zone", method:"POST", action:"save")
+            "/zones"(controller:"Zone", method:"PUT", action:"update")
+            "/zones"(controller:"Zone", method:"DELETE", action:"delete")
 		}
 
-        "/animals"(resources:"AnimalType")
-        "/zonetypes"(resources:"ZoneType")
+        "/animals"(resources:"AnimalType",  excludes:['create', 'show', 'edit', 'patch'])
+        "/zonetypes"(resources:"ZoneType",  excludes:['create', 'show', 'edit', 'patch'])
 
         "/"(view:"/index")
         "500"(view:'/error')
