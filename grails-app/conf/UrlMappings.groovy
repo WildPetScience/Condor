9a567ce19a1d02f4ce025ac93d26353cbc9ecb7e
Nick Rogers <rogersnm@hotmail.co.uk>
2015-02-18 06:01:05 +0000

    Cleanup branch

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -8,10 +8,6 @@ class UrlMappings {
         "/api/animals"(resources:"AnimalType",  excludes:['create', 'show', 'edit', 'patch'])
         "/api/zonetypes"(resources:"ZoneType",  excludes:['create', 'show', 'edit', 'patch'])
 
-        // Put extra URL mappings here. String on the left is the URL, string
-        // on the right is the name of the GSP file without .gsp
-        "/test/"(view:"/test/index")
-
         "/"(view:"/index")
 	}
 }
